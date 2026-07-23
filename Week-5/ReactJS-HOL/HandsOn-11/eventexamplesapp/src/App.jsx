aab6d15ebea6824b576e4736cf0300b2f2047111
harikasinha18 <harikasinhap@ahaicoe.org>
2026-07-23 11:47:01 +0530

    Completed React HandsOn-11

---
+++ Week-5/ReactJS-HOL/HandsOn-11/eventexamplesapp/src/App.jsx
@@ -0,0 +1,77 @@
+import React, { Component } from "react";
+import CurrencyConvertor from "./CurrencyConvertor";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      count: 0
+    };
+  }
+
+  increment = () => {
+    this.setState({
+      count: this.state.count + 1
+    });
+  };
+
+  decrement = () => {
+    this.setState({
+      count: this.state.count - 1
+    });
+  };
+
+  sayHello = () => {
+    alert("Hello! Have a nice day.");
+  };
+
+  handleIncrement = () => {
+    this.increment();
+    this.sayHello();
+  };
+
+  sayWelcome = (message) => {
+    alert(message);
+  };
+
+  handlePress = (event) => {
+    alert("I was clicked");
+  };
+
+  render() {
+    return (
+      <div style={{ padding: "20px" }}>
+        <h1>React Event Examples</h1>
+
+        <h2>Counter: {this.state.count}</h2>
+
+        <button onClick={this.handleIncrement}>
+          Increment
+        </button>
+
+        <button onClick={this.decrement} style={{ marginLeft: "10px" }}>
+          Decrement
+        </button>
+
+        <br /><br />
+
+        <button onClick={() => this.sayWelcome("Welcome")}>
+          Say Welcome
+        </button>
+
+        <br /><br />
+
+        <button onClick={this.handlePress}>
+          OnPress
+        </button>
+
+        <hr />
+
+        <CurrencyConvertor />
+      </div>
+    );
+  }
+}
+
+export default App;
